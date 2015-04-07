70ac1bcb6939eb6c134a3756bd2db105ba3a8c17
iceburg1991 <ijsbrand@esser-emmerik.nl>
2015-04-07 16:40:16 +0200

    lol pagina met Materializecss framework
    Signed-off-by: iceburg1991 <ijsbrand@esser-emmerik.nl>

---
+++ resources/views/lolletje.blade.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+     <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js"></script>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">{{ $title }}</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="sass.html">Sass</a></li>
+                        <li><a href="components.html">Components</a></li>
+                        <li><a href="javascript.html">JavaScript</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
